00f6694cb75b7fede08afa90659cbcd8a619f28e
AndiLam <iilhamm7217@gmail.com>
2025-07-28 01:05:31 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         react(),
     ],
     build: {
-        outDir: '../public/build',
+        outDir: '../public_html/build',
         emptyOutDir: true,
         manifest: true
     },
