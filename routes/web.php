b6727f3875aad8cb15b1b197675faf8ed78ccc87
AndiLam <iilhamm7217@gmail.com>
2025-08-04 03:06:31 +0700

    test notif

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminDashboardController;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use App\Helpers\PushNotification;
 
 
 
@@ -19,6 +20,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/uji-push', function () {
+    PushNotification::send('Judul Uji', 'Isi dari hosting!');
+    return 'Notifikasi dikirim.';
 });
 
 
