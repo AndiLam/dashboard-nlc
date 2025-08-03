f3c8d63ff3d73c4c6f5807d4c15e61ef44157874
AndiLam <iilhamm7217@gmail.com>
2025-08-03 13:29:02 +0700

    logo resize

--- vite.config.js
+++ vite.config.js
@@ -23,12 +23,12 @@ export default defineConfig({
                 theme_color: '#4f46e5',
                 icons: [
                     {
-                        src: '/public/assets/logo.png',
+                        src: '/public/assets/logo-192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: '/public/assets/logo.png',
+                        src: '/public/assets/logo-512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     }
