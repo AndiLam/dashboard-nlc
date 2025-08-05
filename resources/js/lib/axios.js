fcc7e5f750a8c9c847ac87fc9302506dfc4c0faf
AndiLam <iilhamm7217@gmail.com>
2025-08-06 06:47:55 +0700

    cors

--- resources/js/lib/axios.js
+++ resources/js/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_APP_URL || 'https://focfarm.id',
+  baseURL: 'https://focfarm.id',
   withCredentials: true,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
